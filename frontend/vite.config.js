d93088c7d9712cf5674d7107c0ef1564f0a4ca57
David Khurtsidze <ra1nygts@gmail.com>
2026-03-29 13:15:04 +0300

    set prettier, tailwindcss and svelte up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,41 @@
+// import { defineConfig } from 'vite'
+// import { svelte } from '@sveltejs/vite-plugin-svelte'
+//
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [svelte()],
+// })
+
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  base: '/static/svelte/assets/',
+  plugins: [tailwindcss(), svelte()],
+  resolve: {
+    alias: {
+      fonts: resolve('../static/svelte/fonts'),
+      svg: resolve('../static/svelte/svg'),
+    },
+  },
+  build: {
+    outDir: resolve('../main/static/svelte/assets'),
+    assetsDir: '',
+    chunkSizeWarningLimit: 1000,
+    manifest: true,
+    emptyOutDir: true,
+    watch: {},
+  },
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    port: 8000,
+    hot: true,
+  },
+});
+
