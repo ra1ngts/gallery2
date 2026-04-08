cc39b5e11e4761a3a15fbea5a831564b7227729b
David Khurtsidze <ra1nygts@gmail.com>
2026-04-08 15:18:51 +0300

    Added category root in to urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from main.views import index
+from main.views import index, category
 
 app_name = 'main'
 urlpatterns = [
     path('', index, name='index'),
+    path('<slug:slug>/', category, name='category'),
 ]
