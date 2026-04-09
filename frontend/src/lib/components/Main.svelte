98a1bfa12ab769f0c800ebecbd4a2ff981b1bdc6
David Khurtsidze <ra1nygts@gmail.com>
2026-04-09 17:30:03 +0300

    Added swiper styles + fix pagination

--- frontend/src/lib/components/Main.svelte
+++ frontend/src/lib/components/Main.svelte
@@ -17,12 +17,12 @@
     const swiperArtworks = new Swiper(swiperObj, {
       modules: [Navigation, Pagination, Mousewheel],
       slidesPerView: 3,
-      spaceBetween: 30,
+      spaceBetween: 16,
       watchOverflow: true,
       loop: false,
       mousewheel: true,
       pagination: {
-        el: swiperObj.querySelector('.swiper-pagination'),
+        el: swiperObj.closest('.art-work').querySelector('.swiper-pagination-artworks'),
         clickable: true,
       },
       navigation: {
@@ -32,7 +32,7 @@
       breakpoints: {
         320: {
           slidesPerView: 1,
-          spaceBetween: 15,
+          spaceBetween: 16,
           navigation: {
             enabled: false,
           },
@@ -42,7 +42,7 @@
         },
         640: {
           slidesPerView: 2,
-          spaceBetween: 15,
+          spaceBetween: 16,
           navigation: {
             enabled: false,
           },
@@ -52,7 +52,7 @@
         },
         768: {
           slidesPerView: 3,
-          spaceBetween: 15,
+          spaceBetween: 16,
           navigation: {
             enabled: false,
           },
@@ -62,7 +62,7 @@
         },
         1024: {
           slidesPerView: 3,
-          spaceBetween: 15,
+          spaceBetween: 16,
           navigation: {
             enabled: true,
           },
@@ -111,24 +111,32 @@
 </script>
 
 {#each availableCategories as catId}
-  <div class="swiper" use:swiperCreation>
-    <div class="swiper-wrapper">
-      {#each stateCtx.artworks.filter((a) => a.category.id === catId) as artwork}
-        <div class="swiper-slide">
-          <div class="h-80 overflow-hidden rounded-2xl bg-white">
-            <a href={artwork.image} data-fancybox="artwork-{artwork.id}">
-              <img
-                src={artwork.image}
-                alt={artwork.name}
-                class="w-full h-full object-contain cursor-pointer opacity-100 lg:opacity-90 lg:group-hover:opacity-100 transition-opacity duration-300"
-              />
-            </a>
+  <div class="pb-4 art-work">
+    <div class="swiper" use:swiperCreation>
+      <div class="swiper-wrapper">
+        {#each stateCtx.artworks.filter((a) => a.category.id === catId) as artwork}
+          <div class="swiper-slide">
+            <div
+              class="group h-full flex flex-col gap-4 px-4 lg:px-10 py-4 rounded-2xl transition-all duration-300 bg-white/5 shadow-2xl backdrop-blur-md hover:bg-white/5 lg:bg-transparent lg:hover:backdrop-blur-md
+                lg:hover:shadow-2xl lg:hover:shadow-cyan-950/20"
+            >
+              <div class="h-80 overflow-hidden rounded-2xl bg-white">
+                <a href={artwork.image} data-fancybox="artwork-{artwork.id}">
+                  <img
+                    src={artwork.image}
+                    alt={artwork.name}
+                    class="w-full h-full object-cover cursor-pointer opacity-100 lg:opacity-90 lg:group-hover:opacity-100 transition-opacity duration-300"
+                  />
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-      {/each}
+        {/each}
+      </div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
     </div>
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
-    <div class="swiper-pagination"></div>
+
+    <div class="swiper-pagination-artworks"></div>
   </div>
 {/each}
