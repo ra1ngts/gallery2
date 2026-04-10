0827bcf6fa96ac0447d9d8e9032c681a9e7548d7
David Khurtsidze <ra1nygts@gmail.com>
2026-04-10 15:13:50 +0300

    Added Category and Loader

---
+++ frontend/src/lib/components/Loader.svelte
@@ -0,0 +1,24 @@
+<div class="flex h-screen w-full flex-col items-center justify-center">
+  <div class="loader"></div>
+</div>
+
+<style>
+  .loader {
+    margin: auto;
+    width: 150px;
+    height: 150px;
+    border: 2px solid #eae4ea;
+    border-bottom: 2px solid #2a2a2a;
+    border-radius: 50%;
+    animation: loader 2s linear infinite;
+  }
+
+  @keyframes loader {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
