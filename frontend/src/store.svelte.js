177f046f0510894ad0f144ba5d22d676d6234704
David Khurtsidze <ra1nygts@gmail.com>
2026-04-10 15:12:39 +0300

    Rebuilt store.svelte.js

--- frontend/src/store.svelte.js
+++ frontend/src/store.svelte.js
@@ -1,26 +1,16 @@
 // stateCtx
 export const stateCtx = $state({
     artworks: [],
-    menu: [
-        {id: 'main', title: 'Main'},
-        {id: 'category', title: 'Category'},
-        {id: 'about', title: 'About'},
-        {id: 'contact', title: 'Contact'}
-    ],
-    activeMenu: 'main'
+    categories: [],
+    page: '',
+    pages: {
+        loading: 'loading',
+        main: 'main',
+        category: 'category',
+        contact: 'contact'
+    },
+    menu: {
+        main: 'Main',
+        category: 'Category',
+    }
 });
-
-// export const contactForm = (recaptcha_token = null) => {
-//   const form = new FormData();
-//   form.set('name', stateCtx.contactsData.name || '');
-//   form.set('subject', stateCtx.contactsData.subject || '');
-//   form.set('message', stateCtx.contactsData.message || '');
-//   form.set('email', stateCtx.contactsData.email || '');
-
-//   if (recaptcha_token) {
-//     form.set('recaptcha_token', recaptcha_token);
-//   }
-
-//   console.log('contactForm has sent data:', Object.fromEntries(form.entries()));
-//   return form;
-// };
