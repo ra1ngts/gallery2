d75c283ea63f4d32691c56d4fe3f2102d8124384
David Khurtsidze <ra1nygts@gmail.com>
2026-05-30 17:07:00 +0300

    Added en languages translations

--- gallery2/settings.py
+++ gallery2/settings.py
@@ -131,6 +131,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
